ce7dc9cddc17ccc70647e464e72660f5a73895d0
bobromero <rromero123102@gmail.com>
2024-09-04 09:24:31 -0400

    added multiple types of movement

--- movement.gd
+++ movement.gd
@@ -6,20 +6,37 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
+	
+
+	_platformerMovement()	
+
+	move_and_slide()
+
+func _topDownMovement():
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_vector("left", "right", "up", "down")
+	if direction:
+		velocity = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+func _platformerMovement():
+	
 	# Add the gravity.
 	#if not is_on_floor():
 		#velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+		velocity.y = move_toward(velocity.y, 0, SPEED)
