d26c686412308386412d2df83b6636c7faa8cb26
bobromero <rromero123102@gmail.com>
2024-09-04 09:08:19 -0400

    updated player to actually move

--- movement.gd
+++ movement.gd
@@ -7,8 +7,8 @@ const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
