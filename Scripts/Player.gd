caba92d695af34330381a482d94e1b6d2e9ae966
bobromero <rromero123102@gmail.com>
2024-09-14 12:18:35 -0400

    refactor scripts on player to allow for multiple resources including movement and inventory system

---
+++ Scripts/Player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+@export var movement: movement
+@export var Inventory: Inventory
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	movement.SetPlayer(self)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	movement._physics_process(delta)
