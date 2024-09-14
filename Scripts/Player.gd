71b02fc91c0e09bd418b82e158182987b1bcf9b7
bobromero <rromero123102@gmail.com>
2024-09-14 13:28:03 -0400

    add setup for registering attacks

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,11 +3,15 @@ extends CharacterBody2D
 @export var movement: movement
 @export var Inventory: Inventory
 
+@export var attackRegion: Area2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	movement.SetPlayer(self)
-
+	
+func _equipWeapon():
+	Inventory.Weapon.attackRegion = attackRegion
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- Scripts/Weapons/Weapon.gd
+++ Scripts/Weapons/Weapon.gd
@@ -3,6 +3,7 @@ extends Item
 class_name weapon
 
 @export var damage: int = 10
+var attackRegion:Area2D
 
 func _primaryAttack():
 	pass
