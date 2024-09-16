1a0a3ecc1eb9fe7a8fcfa5b34c27854c30ac309d
kevin sturge <crazyguy644@gmail.com>
2024-09-16 10:36:11 -0400

    add functions to remove and add score values

--- hud.gd
+++ hud.gd
@@ -1,10 +1,18 @@
 extends CanvasLayer
 
-var currentscore = 0
 
+@export var currentscore = 0
+
+#function to add 10 to the score
+func addScore(int) -> void:
+	currentscore += int
+	
+func rmScore(int) -> void:
+	currentscore -= int
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$scoreCount.text = "Score: " + str(currentscore) + "00"
+	$scoreCount.text = "Score: " + str(currentscore)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
