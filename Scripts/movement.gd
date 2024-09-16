ba8100a4cc6346d21e8d2391d088116eed249e6e
Kohoholint <valtorres2022@gmail.com>
2024-09-16 12:12:44 -0400

    Redo movement improvements and improved upon jump height

--- Scripts/movement.gd
+++ Scripts/movement.gd
@@ -1,4 +1,4 @@
-extends Resource
+extends Resource 
 
 class_name movement
 
@@ -13,12 +13,13 @@ func SetPlayer(_player: CharacterBody2D):
 	print("here")
 
 
-@onready var coyote_time = $CoyoteTimer 
-@onready var jump_buffer_timer = $JumpBufferTimer
-@onready var jump_height_timer = $JumpHeightTimer
+var coyote_timer = 0.15 #Time in seconds
+var jump_buffer_timer = 0.15
+var jump_height_timer = 0.10
 
 var can_coyote_jump = false
 var jump_buffered = false
+var jump_height_timer_on = false
 
 
 func _physics_process(delta: float) -> void:
@@ -39,23 +40,29 @@ func _topDownMovement():
 		player.velocity.x = move_toward(player.velocity.x, 0, SPEED)
 		player.velocity.y = move_toward(player.velocity.y, 0, SPEED)
 
-	move_and_slide()
+	player.move_and_slide()
 
 
 
 func _platformerMovement(delta: float):
+	if jump_buffered:
+		_jump_buffer_timer(delta)
+		
+	if can_coyote_jump:
+		_coyote_timer(delta)
+		
+	if jump_height_timer_on:
+		_jump_height_timer(delta)
+		
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-		if velocity.y > 1000:
-			velocity.y = 1000
-	# Handle jump.
-	if Input.is_action_just_pressed("up") and player.is_on_floor():
-		player.velocity.y = JUMP_VELOCITY
+	if not player.is_on_floor():
+		player.velocity += player.get_gravity() * delta
+		if player.velocity.y > 1000:
+			player.velocity.y = 1000
 	
 	# Handle jump.
 	if Input.is_action_just_pressed("up"):
-		jump_height_timer.start()
+		_jump_height_timer_start()
 		jump()
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -63,46 +70,72 @@ func _platformerMovement(delta: float):
 	if direction:
 		player.velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		player.velocity.x = move_toward(player.velocity.x, 0, SPEED)
 		
-	var was_on_floor = is_on_floor()
-	move_and_slide()
+	var was_on_floor = player.is_on_floor()
+	player.move_and_slide()
 	
 	#Play starts falling
-	if was_on_floor and !is_on_floor() and velocity.y >= 0:
+	if was_on_floor and !player.is_on_floor() and player.velocity.y >= 0:
 		can_coyote_jump = true
-		coyote_time.start()
 		
 	#Player has tounched ground
-	if !was_on_floor and is_on_floor():
+	if !was_on_floor and player.is_on_floor():
 		if jump_buffered:
 			jump_buffered = false
 			print("Buffered jump")
 			jump()
 
 func jump():
-	if is_on_floor() || can_coyote_jump:
+	if player.is_on_floor() || can_coyote_jump:
 		#Gives you a grace period to jump off the edge of a platform
-		velocity.y = JUMP_VELOCITY
+		player.velocity.y = JUMP_VELOCITY
 		if can_coyote_jump:
 			can_coyote_jump = false
 			print("Coyote jump")
 	else:
 		if !jump_buffered:
 			jump_buffered = true
-			jump_buffer_timer.start()
 			
+			
+			
+func _coyote_timer(delta) -> void:
+	coyote_timer -= delta
+	if coyote_timer <= 0:
+		_on_coyote_timer_timeout()
+		
+		
+func _jump_buffer_timer(delta) -> void:
+	jump_buffer_timer -= delta
+	if jump_buffer_timer <= 0:
+		_on_jump_buffer_timer_timeout()
+		
+		
+func _jump_height_timer_start():
+	if jump_height_timer == 0.0:
+		jump_height_timer = 0.10
+		
+	jump_height_timer_on = true
+
+
+
+func _jump_height_timer(delta) -> void:
+	jump_height_timer -= delta
+	if jump_height_timer <= 0:
+		_on_jump_height_timer_timeout()
 			
 func _on_coyote_timer_timeout() -> void:
 	can_coyote_jump = false
+	coyote_timer = 0.15
 
 func _on_jump_buffer_timer_timeout() -> void:
 	jump_buffered = false
+	jump_buffer_timer = 0.15
 
 func _on_jump_height_timer_timeout() -> void:
 	if !Input.is_action_pressed("up"):
-		if velocity.y < -90:
-			velocity.y = -90
+		if player.velocity.y < -90:
+			player.velocity.y = -90
 	else:
 		pass
-		player.velocity.x = move_toward(player.velocity.x, 0, SPEED)
+		
