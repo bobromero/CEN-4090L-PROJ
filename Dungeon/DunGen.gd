0762aa3b29251e90b78486856ac7b6888b026c72
bobromero <rromero123102@gmail.com>
2024-09-23 12:40:57 -0400

    add setup for room gen

--- Dungeon/DunGen.gd
+++ Dungeon/DunGen.gd
@@ -1,7 +1,9 @@
 extends TileMapLayer
 
 
+class_name DunGen
 # Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	for x in 10:
 		for y in 10:
@@ -12,3 +14,18 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+
+func _buildRoom(room : Room)-> bool:
+	
+	
+	
+	return true
+
+#rooms defined as 
+
+# rooms are made of 1 or more rectangles
+# if there is a function for building rooms, and the relative position of the doors are matching
+#we should be able to run another room
+# connect rooms with hallways
--- Dungeon/Room.gd
+++ Dungeon/Room.gd
@@ -1,19 +1,8 @@
+extends Resource
 class_name Room
 
-var id: int
-var is_starting_room: bool = false
-var connected_rooms: Array = []
+var startPos : Vector2
+var rectangels : Array[Vector2]
 
-func _init(new_id: int, starting_room: bool = false): 
-	id = new_id
-	is_starting_room = starting_room
-	if is_starting_room:             #Create passive starting room 
-		new_id = 0 
-		connected_rooms = []
-
-func connect_room(room: Room):       #Connects rooms to each other
-	connected_rooms.append(room)
-
-func generate_random_room():         #Randomly selects from the premade item pool
-	var room = Room.new(randi()) 
-	return room
+var connectionPoints: Array[Vector2]
+var Dimensions: Vector2
