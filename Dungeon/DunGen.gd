c823527b13a8033b27e0e758f1e94962df2b1348
bobromero <rromero123102@gmail.com>
2024-10-04 14:32:53 -0400

    add room generation with borders

--- Dungeon/DunGen.gd
+++ Dungeon/DunGen.gd
@@ -3,19 +3,36 @@ extends TileMapLayer
 
 class_name DunGen
 # Called when the node enters the scene tree for the first time.
-
+var start:Vector2i
 func _ready() -> void:
-	for x in 10:
-		for y in 10:
-			print(x,y)
-			set_cell(Vector2i(x,y), 0, Vector2i(1,1), 0)
+	start = Vector2i(0,0)
+	for x in 8:
+		start = _buildRect(start, randi_range(3,15), randi_range(3,7)) + Vector2i(randi_range(-2,-1), randi_range(-2,-1))
+
+	
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("PrimaryFire"):
+		start = _buildRect(start, randi_range(3,15), randi_range(3,7)) + Vector2i(randi_range(-2,-1), randi_range(-2,-1))
+	
+	
 
-
+func _buildRect(startCoord:Vector2i, sizeX:int, sizeY:int) -> Vector2i:
+	var background:Vector2i = Vector2i(1,1) 
+	var wall:Vector2i = Vector2i(3,2) 
+	for x in sizeX:
+		for y in sizeY:
+			var pos: Vector2i = Vector2i(x,y)
+			var isEdge:bool = (pos.x == 0 or pos.x == sizeX-1)
+			var isWall:bool = (pos.y == 0 or pos.y == sizeY-1)
+			if isEdge or isWall:
+				set_cell(pos + startCoord, 0, wall, 0)
+			else:
+				set_cell(pos + startCoord, 0, background, 0)
+	return startCoord + Vector2i(sizeX, sizeY)
 
 func _buildRoom(room : Room)-> bool:
 	
