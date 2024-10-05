2733e4626f17e01225ff2f1127f91c21092b8713
bobromero <rromero123102@gmail.com>
2024-10-05 18:34:03 -0400

    changed helath item to use new OnPickup function

--- Scripts/Items/HealthItem.gd
+++ Scripts/Items/HealthItem.gd
@@ -2,7 +2,12 @@ extends Item
 
 @export var amount: float
 
+func OnPickup(player: Player):
+	super(player)
+	Use(player)
+	
+
 func Use(player: Player):
 	super(player)
 	player.IncreaseHealth(amount)
-	print(str("increased health to " , player.health))
+	#print(str("increased health to " , player.health))
