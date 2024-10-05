ed1b45959bc44b5e1e4e2b37dfd28d1511ac0288
bobromero <rromero123102@gmail.com>
2024-10-05 19:06:53 -0400

    added invID to identify each item

--- Scripts/Items/Item.gd
+++ Scripts/Items/Item.gd
@@ -7,10 +7,10 @@ enum ItemType {Weapon, Health, Buff, Currency}
 
 @export var name: String = ""
 @export var type: ItemType = ItemType.Weapon
+@export var InvId: int = -1 # -1 means not in an inventory
 
 func OnPickup(player: Player):
 	pass
 
 func Use(player: Player):
-	player.RemoveFromInventory()
 	pass
