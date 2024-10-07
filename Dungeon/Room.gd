05ad0d05482a2b0a9044c4d6f6fcb661b4127b2f
bobromero <rromero123102@gmail.com>
2024-10-07 15:17:53 -0400

    add var for door locations

--- Dungeon/Room.gd
+++ Dungeon/Room.gd
@@ -5,6 +5,8 @@ var startPos : Vector2i
 
 var Dimensions: Vector2i
 
+var Doors:Array[Vector2i]
+
 func _init(_startPos: Vector2i, _dimensions : Vector2i):
 	startPos = _startPos
 	Dimensions = _dimensions
