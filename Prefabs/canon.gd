eda4ea468eaa47b5287b8bfe346493bc2450c8ca
kevin sturge <crazyguy644@gmail.com>
2024-11-18 10:31:16 -0500

    added projectile and script

---
+++ Prefabs/canon.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready var ProjectileShooter = get_tree().get_root().get_node("ProjectileShooter")
+@onready var projectile = load("res://Scenes/Projectile.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	shoot()
+	pass # Replace with function body.
+
+func shoot():
+	var instance = projectile.instantiate()
+	instance.direction = rotation
+	instance.initialPos = global_position
+	instance.initialRot = rotation
+	ProjectileShooter.add_child.call_deferred(instance)
+	
--- Scripts/Weapons/projectile.gd
+++ Scripts/Weapons/projectile.gd
@@ -1,25 +1,17 @@
 extends CharacterBody2D
 
+@export var SPEED = 100
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var direction : float
 
+var initialPos : Vector2
 
+var initialRot : float
+
+func _ready(): 
+	global_position = initialPos
+	global_rotation = initialRot
+	
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	velocity = Vector2(0, -SPEED).rotated(direction)
 	move_and_slide()
