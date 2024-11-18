e572065f60581cc62786e9bf2d0561e39a49d786
bobromero <rromero123102@gmail.com>
2024-11-18 12:46:47 -0500

    bruh idk

--- Dungeon/Dungen.cs
+++ Dungeon/Dungen.cs
@@ -4,127 +4,262 @@ using System.Collections.Generic;
 using System.Linq;
 
 public partial class Dungen : Node2D {
-    
-    public class Dungeon {
-        public String[] StartPaths = new string[] {
+	
+	public class Dungeon {
+		public String[] StartPaths = new string[] {
 			"res://Prefabs/Rooms/room0.tscn",
 			"res://Prefabs/Rooms/room1.tscn"
 		};
-        public String[] CommonPaths = new string[] {
-            "res://Prefabs/Rooms/room0.tscn",
-            "res://Prefabs/Rooms/room1.tscn"
-        };
-        public String[] LootPaths = new string[] {
-            "res://Prefabs/Rooms/room0.tscn",
-            "res://Prefabs/Rooms/room1.tscn"
-        };
-        public String[] BossPaths = new string[] {
-            "res://Prefabs/Rooms/room0.tscn",
-            "res://Prefabs/Rooms/room1.tscn"
-        };
-		public Vector2I spawnOffset = new Vector2I(0, 0);
-        Room StartRoom;
+		public String[] CommonPaths = new string[] {
+			"res://Prefabs/Rooms/room0.tscn",
+			"res://Prefabs/Rooms/room1.tscn"
+		};
+		public String[] LootPaths = new string[] {
+			"res://Prefabs/Rooms/room0.tscn",
+			"res://Prefabs/Rooms/room1.tscn"
+		};
+		public String[] BossPaths = new string[] {
+			"res://Prefabs/Rooms/room0.tscn",
+			"res://Prefabs/Rooms/room1.tscn"
+		};
+		Room StartRoom;
 		public int roomCount = 0;
 		public int MaxDepth; // how long before force terminating a path
 		public bool hasBoss = false;
-		public Node Host;
+		public Node HostNode;
 
 		public Dungeon(Node host, int maxDepth) {
-			Host = host;
-            MaxDepth = maxDepth;
-            TileMapLayer Layer = (TileMapLayer)GD.Load<PackedScene>(StartPaths[0]).Instantiate();
-            Host.AddChild(Layer);
-            Room room = new Room(Layer, roomCount++, Room.RoomType.Start);
-			room.GenerateNeighbor(this, 0);
-        }
+			HostNode = host;
+			MaxDepth = maxDepth;
+			TileMapLayer Layer = (TileMapLayer)GD.Load<PackedScene>(StartPaths[0]).Instantiate();
+			HostNode.AddChild(Layer);
+			Room.Door door = new Room.Door {
+				offset = Layer.GetChild(0).GetChild<Node2D>(0).Position,
+				direction = Room.Door.Direction.North,
+				GlobalPos = new Vector2(0, 0)
+			};
+
+			Room room = new Room(Layer, roomCount++, door);
+			room.GenerateNeighbors(this, room, door, 0);
+		}
 	}
 
-    public class Room
-    {
-        public enum RoomType{
+	public class Room
+	{
+		public enum RoomType{
 			Start, // start room
 			Common, // enemy room
 			Loot, //end room
 			Boss // end boss room, only 1
 
-        }
-		public RoomType Type;
-        public Node Self;
-        private int id;
-		private IEnumerable<Node> doors;
-		public IEnumerable<Node> Doors {
+		}
+		public class Door
+		{
+			public enum Direction {
+				North,
+				South,
+				East,
+				West
+			}
+			public Direction direction;
+			public Vector2 GlobalPos;
+			public Vector2 offset;
+		}
+		public Node2D Self;
+		private int id;
+
+		private Door[] doors;
+		public Door[] Doors {
 			private set { this.doors = value; }
 			get { return this.doors; }
 		}
-		private List<Room> Neighbors = new List<Room>();
-	
-		public Room(Node self, int _id, RoomType type) {
-            Self = self;
+
+		private Room[] Neighbors;
+		private Door Connector;
+
+
+		public Room(Node2D self, int _id, Door connector) {
+			Self = self;
 			id = _id;
-			Type = type;
 			doors = GetDoors(self);
+			Connector = connector;
 		}
 
-		public IEnumerable<Node> GetDoors(Node Scene) {
-			return Scene.GetChildren().Where(r => r.GetChild(0).Name.ToString().Contains("Door"));
-        }
-        
-        public void GenerateNeighbor(Dungeon d, int depth) {
-            if (depth > d.MaxDepth) {
-                return;
-            }
-            for (int i = 0; i < doors.Count(); i++) {
-                RoomType type;
-                
-                if (depth > d.MaxDepth-1) {
-                    if (d.hasBoss)
-                    {
-						type = RoomType.Loot;
-                        
-					} else {
-						type = RoomType.Boss;
-                        d.hasBoss = true;
-                    }
-                }
-                else
-                {
-					type = RoomType.Common;
-                }
-                String path = "";
-                
-                Random rand = new Random();
-                switch (type) {
-                    case RoomType.Common:
-                        path = d.CommonPaths[rand.Next(0, d.CommonPaths.Count())];
-                        GD.Print("Spawning Common");
-                        break;
-                    case RoomType.Loot:
-                        path = d.LootPaths[rand.Next(0, d.LootPaths.Count())];
-                        GD.Print("Spawning Loot");
-                        break;
-                    case RoomType.Boss:
-                        path = d.BossPaths[rand.Next(0, d.BossPaths.Count())];
-                        GD.Print("Spawning Boss");
-                        break;
-                    default:
-                        break;
-                }
-                TileMapLayer scene = (TileMapLayer)GD.Load<PackedScene>(path).Instantiate();
-                d.Host.AddChild(scene);
-                d.spawnOffset += scene.GetUsedRect().Position * 500;
-                Room room = new Room(scene, d.roomCount++, type);
-                Neighbors.Add(room);
+		public Door[] GetDoors(Node Scene) {
+			Node[] input = Scene.GetChildren().Where(r => r.GetChild(0).Name.ToString().Contains("Door")).ToArray();
+			Door[] result = new Door[input.Count()];
 
-            }
-            foreach (var neighbor in Neighbors)
-            {
-                depth+=1;
-                neighbor.GenerateNeighbor(d, depth);
-            }
+			for (int i = 0; i < input.Count(); i++)
+			{
+				Door.Direction pos = Door.Direction.North;
 
-        }
+				switch (input[i].Name.ToString()[0]) {
+					case 'N':
+						pos = Door.Direction.North;
+						break;
+					case 'E':
+						pos = Door.Direction.East;
+						break;
+					case 'S':
+						pos = Door.Direction.South;
+						break;
+					case 'W':
+						pos = Door.Direction.West;
+						break;
+					default:
+						break;
+				}
+				var _offset = input[i].GetChild<Node2D>(0).Position;
+				GD.Print(input[i].GetChild<Node2D>(0).Name.ToString());
+				result[i] = new Door {
+					direction = pos,
+					offset = _offset,
+					GlobalPos = _offset
+				};
+			}
+			return result.ToArray();
+		}
+		
+		public void GenerateNeighbors(Dungeon d, Room ParentRoom, Door connectedDoor, int depth) {
+			if (depth >= d.MaxDepth) {
+				return;
+			}
+			Neighbors = new Room[doors.Count()];
+			Neighbors[0] = ParentRoom;
 
-    }
+			Door CorrespondingDoor = new Door();
+			switch (connectedDoor.direction) {
+				case Door.Direction.North:
+					CorrespondingDoor.direction = Door.Direction.South;
+					break;
+				case Door.Direction.South:
+					CorrespondingDoor.direction = Door.Direction.North;
+					break;
+				case Door.Direction.East:
+					CorrespondingDoor.direction = Door.Direction.West;
+					break;
+				case Door.Direction.West:
+					CorrespondingDoor.direction = Door.Direction.East;
+					break;
+				default:
+					break;
+			}
+
+			for (int i = 0; i < doors.Count(); i++)
+			{
+				if (doors[i].direction == CorrespondingDoor.direction)
+				{
+					CorrespondingDoor.offset = doors[i].offset;
+				}
+			}
+			for (int i = 1; i < doors.Count(); i++) {
+				var scene = SpawnRoom(d, depth);
+				d.HostNode.AddChild(scene);
+
+
+
+				scene.GlobalPosition = connectedDoor.GlobalPos;
+
+				Room room = new Room(scene, d.roomCount++, doors[i]);
+
+				GD.Print("Corresponding Door " + CorrespondingDoor.GlobalPos);
+				GD.Print("Room Pos " + room.Self.GlobalPosition);
+
+				doors[i].GlobalPos = scene.GlobalPosition + doors[i].offset;
+
+				Neighbors[i] = room;
+				Neighbors[i].GenerateNeighbors(d, this, doors[i], ++depth);
+			}
+		}
+
+
+		private TileMapLayer SpawnRoom(Dungeon d,int depth) {
+			RoomType type;
+
+			if (depth > d.MaxDepth - 1) {
+				if (d.hasBoss) {
+					type = RoomType.Loot;
+
+				} else {
+					type = RoomType.Boss;
+					d.hasBoss = true;
+				}
+			} else {
+				type = RoomType.Common;
+			}
+
+			String path = "";
+
+			Random rand = new Random();
+			switch (type) {
+				case RoomType.Common:
+					path = d.CommonPaths[rand.Next(0, d.CommonPaths.Count())];
+					// GD.Print("Spawning Common");
+					break;
+				case RoomType.Loot:
+					path = d.LootPaths[rand.Next(0, d.LootPaths.Count())];
+					// GD.Print("Spawning Loot");
+					break;
+				case RoomType.Boss:
+					path = d.BossPaths[rand.Next(0, d.BossPaths.Count())];
+					// GD.Print("Spawning Boss");
+					break;
+				default:
+					break;
+			}
+			return (TileMapLayer)GD.Load<PackedScene>(path).Instantiate();
+			
+		}
+
+		//public void GeneratNeighbors(Dungeon d, Room ParentRoom, Door connectedDoor, int depth) {
+		//	if (depth >= d.MaxDepth) {
+		//		return;
+		//	}
+		//	Neighbors = new Room[doors.Count()];
+		//	Neighbors[0] = ParentRoom;
+
+		//	Door CorrespondingDoor = new Door();
+		//	switch (connectedDoor.direction) {
+		//		case Door.Direction.North:
+		//			CorrespondingDoor.direction = Door.Direction.South;
+		//			break;
+		//		case Door.Direction.South:
+		//			CorrespondingDoor.direction = Door.Direction.North;
+		//			break;
+		//		case Door.Direction.East:
+		//			CorrespondingDoor.direction = Door.Direction.West;
+		//			break;
+		//		case Door.Direction.West:
+		//			CorrespondingDoor.direction = Door.Direction.East;
+		//			break;
+		//		default:
+		//			break;
+		//	}
+		//	CorrespondingDoor.GlobalPos = new Vector2(0, 0);
+		//	CorrespondingDoor.GlobalPos += connectedDoor.offset;
+		//	CorrespondingDoor.offset = (doors.Where(door => door.direction == CorrespondingDoor.direction) as Door).offset;
+
+		//          for (int i = 1; i < doors.Count(); i++) {
+		//              var scene = SpawnRoom(d, depth);
+		//              d.HostNode.AddChild(scene);
+
+		//              scene.GlobalPosition = connectedDoor.GlobalPos + CorrespondingDoor.offset;
+
+		//              Room room = new Room(scene, d.roomCount++, doors[i]);
+
+		//              //GD.Print("Corresponding Door " + CorrespondingDoor.offset + " " +  CorrespondingDoor.direction);
+		//              GD.Print("Room Pos " + room.Self.GlobalPosition);
+
+		//              Neighbors[i] = room;
+		//          }
+		//          for (int i = 1; i < Neighbors.Length; i++) {
+		//              depth++;
+		//              Neighbors[i].GenerateNeighbors(d, this, doors[i], depth);
+		//          }
+		//      }
+
+
+	}
 
 
 	List<Room> rooms = new List<Room>();
@@ -135,17 +270,15 @@ public partial class Dungen : Node2D {
 	public override void _Ready()
 	{
 		
-		dungeon = new Dungeon(this, 4);
-    }
+		dungeon = new Dungeon(this, 2);
+	}
 
-	
-	int roomCount = 0;
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		if (Input.IsActionJustPressed("PrimaryFire")) {
 			GD.Print("Hello");
-            
+			
 		}
 	}
 
@@ -155,4 +288,16 @@ public partial class Dungen : Node2D {
 	 */
 
 
+
+	//spawn room
+	//add offset to room
+	//that offset is the local position of the corresponding door
+	//send down and repeat
+
+	//room spawns at 0,0
+	//spawn new room
+	//new room spawns at 0,0
+	//move new room to room.connectedDoor.doorGlobalPos
+	//add new room's corresponding doorPos offset
+	//repeat
 }
