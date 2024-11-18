6187393af770402b0a1e025969c3583ac0333bb2
kevin sturge <kevinsturge57@gmail.com>
2024-11-17 23:22:05 -0500

    minor title screen changes + pause menu changes

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -23,6 +23,7 @@ func PauseTest():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+@warning_ignore("unused_parameter")
 func _process(delta: float) -> void:
 	PauseTest()
 	pass
