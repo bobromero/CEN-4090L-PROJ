46422efe99d245c3b558f6eb9b07cbf2e0e2d150
kevin sturge <crazyguy644@gmail.com>
2024-12-02 11:28:00 -0500

    working on implementing projectile to player commit

--- Prefabs/canon.gd
+++ Prefabs/canon.gd
@@ -5,8 +5,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	shoot()
 	pass # Replace with function body.
+	
+func _process(delta: float) -> void:	
+	if Input.is_action_just_pressed("shoot"): #shoots when spacebar is hit
+		shoot()
+	pass
 
 
 
