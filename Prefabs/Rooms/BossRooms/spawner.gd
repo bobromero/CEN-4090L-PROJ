2fea71bdd4a26843e7e6883593fcc718bbb0aaed
kevin sturge <kevinsturge57@gmail.com>
2024-12-11 13:57:24 -0500

    Added enemies spawning in the boss room

---
+++ Prefabs/Rooms/BossRooms/spawner.gd
@@ -0,0 +1,30 @@
+extends Node2D 
+
+@onready var enemy= preload("res://Prefabs/Enemy.tscn")
+
+var canSpawn = true
+
+func _ready():
+	pass
+
+func _process(_delta: float) -> void:
+	if Global.isInBossFight == true and canSpawn == true:
+		spawn_enemy()
+		canSpawn = false
+		await get_tree().create_timer(15.0).timeout
+		canSpawn = true
+		
+
+func spawn_enemy():
+	var newEnemy = enemy.instantiate()
+	
+	
+	var randomOffset = Vector2(
+		randf_range(-500, 500), 
+		randf_range(-500, 500)   
+	)
+	
+	
+	newEnemy.global_position = global_position + randomOffset
+	
+	get_tree().current_scene.add_child(newEnemy)
